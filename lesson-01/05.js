1d0c3be50e2c88ef4c1cf014e0d297a3ee8c9f84
reshetniak_vera/weblayout <vera.reshetnyak.1991@yandex.ru>
2023-11-11 12:21:11 +0300

    les1

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -7,7 +7,7 @@ switch (true) {
     console.log("Все таки нет правил без исключения");
     break;
   case rule.length == 25:
-    console.log("Все таки нет правил без исключения");
+    console.log("50 на 50");
     break;
     case rule.length > 25:
       console.log("У меня 100 пудов все получится");
