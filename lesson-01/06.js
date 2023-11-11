9b1fa628132c6341c8e56f3fb03bed076e4653ad
reshetniak_vera/weblayout <vera.reshetnyak.1991@yandex.ru>
2023-11-11 13:44:45 +0300

    les

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -2,3 +2,14 @@
 
 // Создай программу для let str = "zzzzyzz"; используя тернарный оператор и метод includes. 
 // Если нашел букву "y", то отконсоль "нашел", если не нашел, то"заблудился"
+
+// git add .
+// git commit -m "сообщ"
+
+let str = "zzzzyzz";
+
+if (str.includes("y")){
+  console.log("нашел");}
+  else {
+console.log("заблудился");
+  }
