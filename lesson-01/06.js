b0a8069022d09691e5dcb20c9047629104d2a445
reshetniak_vera/weblayout <vera.reshetnyak.1991@yandex.ru>
2023-11-11 14:18:08 +0300

    les1

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,9 +1,10 @@
 
 
-let str = "zzzzyzz";
+let str = "zzzzyzz".includes("y") ? "нашел": "заблудился";
+console.log (str);
 
-if (str.includes("y")){
-  console.log("нашел");}
-  else {
-console.log("заблудился");
-  }
+// if (str.includes("y")){
+//   console.log("нашел");}
+//   else {
+// console.log("заблудился");
+//   }
